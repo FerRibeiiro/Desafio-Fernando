7e60c3ab14a05d41f70daae02f0d74f0ae878f5e
FerRibeiiro <fernando.rib94@gmail.com>
2025-10-30 21:09:13 -0300

    Projeto iniciado

---
+++ index.js
@@ -0,0 +1,8 @@
+//Nome do Meu Heroi
+let nomeHeroi = "Hernesto"
+
+//Perguntando nome e se apresentando
+console.log("Faça uma saudação e diga o nome do seu Herói")
+console.log(`Ola, meu nome é ${nomeHeroi}`)
+
+//Avaliando a porcentagem de farm XP
