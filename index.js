30fb87325c9e3526686163816eb1362646ca52c0
FerRibeiiro <fernando.rib94@gmail.com>
2025-11-05 14:42:05 -0300

    Termino da atividade

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 //Nome do Meu Heroi
-let nomeHeroi = "Hernesto"
+let nomeHeroi = "Hernesto";
 
-//Perguntando nome e se apresentando
-console.log("Faça uma saudação e diga o nome do seu Herói")
-console.log(`Ola, meu nome é ${nomeHeroi}`)
+//Quantidade de XP que o Heroi possui
+let xp = 9999;
 
-//Avaliando a porcentagem de farm XP
+//Analise de nível do personagem
+let nivel = "";
+
+//Avaliar o nível em que o Heroi está
+if(xp < 1000){
+    nivel = "Ferro";
+}else if (xp < 2000){
+    nivel = "Bronze";
+}else if (xp < 5000){
+    nivel = "Prata";
+}else if (xp < 7000){
+    nivel = "Ouro";
+}else if (xp < 8000){
+    nivel = "Platina";
+}else if (xp < 9000){
+    nivel = "Ascendente";
+}else if (xp < 10000){
+    nivel = "Imortal";
+}else {
+    nivel = "Radiante";
+}
+
+//Mensagem de saída
+console.log (`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
