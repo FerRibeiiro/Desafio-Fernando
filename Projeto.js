011e9f5a54812f916eb799d836dfdd1125b66d9e
FerRibeiiro <fernando.rib94@gmail.com>
2025-12-03 11:44:40 -0300

    Correção de layout

--- Projeto.js
+++ Projeto.js
@@ -1,18 +1,18 @@
 // Calculo de partida Rankeadas
-function calculoRankeada(vitorias , derrotas) {
+function calculoRankeada(vitorias, derrotas) {
     // Variavel de saldo de vitórias
-    let saldoVitrorias = 250 - 40
+    let saldoVitrorias = vitorias - derrotas
     // Variavel de nivel de Rankeada
     let nivel = ""
 
     //Decisão de Rankeada
-    if (vitorias =10 ){
+    if (vitorias = 10) {
         nivel = "Ferro"
-    }else if (vitorias >=11 && vitorias <=20){
+    } else if (vitorias >= 11 && vitorias <= 20) {
         nivel = "Bronze"
-    }else if (vitorias >=21 && vitorias <=50){
+    } else if (vitorias >= 21 && vitorias <= 50) {
         nivel = "Prata"
-        } else if (vitorias >= 51 && vitorias <= 80) {
+    } else if (vitorias >= 51 && vitorias <= 80) {
         nivel = "Ouro"
     } else if (vitorias >= 81 && vitorias <= 90) {
         nivel = "Diamante"
@@ -21,7 +21,7 @@ function calculoRankeada(vitorias , derrotas) {
     } else if (vitorias >= 101) {
         nivel = "Imortal"
     }
-    
-// Resultado Rankeada
-    console.log `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`
+
+    // Resultado Rankeada
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`
 }
